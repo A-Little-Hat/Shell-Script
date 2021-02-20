f591962ddafd1501f96ed0d434c84a6082a5accd
Soumyaneel Sarkar <65025579+A-Little-Hat@users.noreply.github.com>
2021-02-20 18:57:33 +0530

    prime check

---
+++ 12.prime.sh
@@ -0,0 +1,16 @@
+echo -n "Enter a number: "
+read num
+i=2
+
+while [ $i -lt $num ]
+do
+  if [ `expr $num % $i` -eq 0 ]
+  then
+      echo "$num is not a prime number"
+      echo "Since it is divisible by $i"
+      exit
+  fi
+  i=`expr $i + 1`
+done
+
+echo "$num is a prime number "
