187e44fcdbdca671127583650aafe6c24a83984a
Soumyaneel Sarkar <65025579+A-Little-Hat@users.noreply.github.com>
2021-02-20 18:54:38 +0530

    pyramid pattern

---
+++ 8.pattern1.sh
@@ -0,0 +1,20 @@
+echo "enter the no of row "
+read n
+i=1
+while [ $i -le $n ]
+    do
+        j=1
+        while [ $j -le $(($n-$i)) ]
+            do
+                echo -n " "
+                j=$(($j+1))
+            done
+        k=1
+        while [ $k -le $i ]
+            do
+                echo -n "* "
+                k=$(($k+1))
+            done
+        echo 
+        i=$(($i+1))
+    done
