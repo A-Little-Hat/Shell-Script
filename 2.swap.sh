053bda774b6af49486e14d49189fbb5a840b154c
Soumyaneel Sarkar <65025579+A-Little-Hat@users.noreply.github.com>
2021-02-28 17:53:24 +0530

    some echo lines edited

--- 2.swap.sh
+++ 2.swap.sh
@@ -2,9 +2,13 @@ echo "enter first number"
 read a
 echo "enter second number"
 read b
-echo "a before swapping is $a and b is $b"
+echo "***before swapping***"
+echo "a = $a"
+echo "b = $b"
 #swapping
 a=$((a+b))
 b=$((a - b))
 a=$((a-b))
-echo "a after swapping is  $a and b is $b"
+echo "***after swapping***
+echo "a = $a"
+echo "b = $b"
