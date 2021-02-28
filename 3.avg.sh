a92a2d46eb138b86c3ed8c08f937c9a32c7e2cd8
A-Little-Hat <soumyasarkar309@gmail.com>
2021-02-28 18:13:27 +0530

    avg updated

--- 3.avg.sh
+++ 3.avg.sh
@@ -15,17 +15,18 @@ echo Enter the Mark5
 read m5
 tot=$(expr $m1 + $m2 + $m3 + $m4 + $m5)
 avg=$(expr $tot / 5)
-echo -----------------------------------
-echo '\tStudent Mark List'
-echo -----------------------------------
-echo "Student Name    : $name"
-echo "Mark1           : $m1"
-echo "Mark2           : $m2"
-echo "Mark3           : $m3"
-echo "Mark4           : $m4"
-echo "Mark5           : $m5"
-echo "Total           : $tot"
-echo "Average         : $avg"
+# echo -----------------------------------
+# echo '\tStudent Mark List'
+# echo -----------------------------------
+# echo "Student Name    : $name"
+# echo "Mark1           : $m1"
+# echo "Mark2           : $m2"
+# echo "Mark3           : $m3"
+# echo "Mark4           : $m4"
+# echo "Mark5           : $m5"
+echo "Total = $tot"
+echo "Average = $avg"
+echo -n "the grade of the student $name is "
 if [ $avg -ge 75 ]; then
     echo "Grade           : A+"
 elif [ $avg -ge 60 ]; then
